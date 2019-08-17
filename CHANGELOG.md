aad0312d2bb083b96bbbf0af2b70d06a9c7647c8
Juan Gomez <j.gomez@drexed.com>
2019-08-17 12:00:40 -0400

    Add monkey patch check

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.1] - 2019-08-17
+### Changed
+- Add check for monkey patches
+
 ## [1.0.0] - 2019-07-19
 ### Added
 - Initial project version
--- lib/lite/measurements/monkey_patches.rb
+++ lib/lite/measurements/monkey_patches.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
-class Numeric
+if Lite::Measurements.configuration.monkey_patches
+  class Numeric
 
-  {
-    digital_storage: Lite::Measurements::DigitalStorage,
-    length: Lite::Measurements::Length,
-    mass: Lite::Measurements::Mass,
-    temperature: Lite::Measurements::Temperature,
-    time: Lite::Measurements::Time
-  }.each do |name, klass|
-    define_method("convert_#{name}") do |from:, to:|
-      klass.convert(self, from: from, to: to)
+    {
+      digital_storage: Lite::Measurements::DigitalStorage,
+      length: Lite::Measurements::Length,
+      mass: Lite::Measurements::Mass,
+      temperature: Lite::Measurements::Temperature,
+      time: Lite::Measurements::Time
+    }.each do |name, klass|
+      define_method("convert_#{name}") do |from:, to:|
+        klass.convert(self, from: from, to: to)
+      end
     end
-  end
 
+  end
 end
